ee7bed54c4d38c2c5d668b3d9fca5677ad1008e8
Fakox <francisco.cantillanao@gmail.com>
2015-04-04 01:55:11 -0300

    algo

--- mreformular.py
+++ mreformular.py
@@ -1,6 +1,16 @@
 import tweepy
 import sys
-from tweepy import Curs|
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+consumer_key='M5WaMYqVPjODgKMbWhVwnBRGA'
+consumer_secret='Z5xMhSrj4P1zerBuFKjAUkyBvhGeypmgRnZZ5ynTlhUlaAbx8H'
+access_token='313642562-xhZu0SdkXFWjIzLf4poaTZe9J8TPDyDYsCjowEvI'
+access_token_secret='kcW1DmYcp4EhIjX7v0v15U8l2hYDtPjfr24Mah5e7OTxo'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
 api = tweepy.API(auth)
 
 # public_tweets = api.home_timeline()
@@ -19,4 +29,9 @@ for x in xrange(1,10):
  #   print tweet.created_at, tweet.text
 #for tweet in c
 #	print tweet.text
+
+
+
+#hi=api.followers_ids('Fakoxx')
+#print hi
 
