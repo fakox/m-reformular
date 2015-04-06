e770cba987acb2fcbb45c8e8d0a8e19ef2e79559
Fakox <francisco.cantillanao@gmail.com>
2015-04-05 23:14:47 -0300

    Ahora selecciona la palabra del '.txt' y encuentra las palabras relacionadas, falta elegir la estructura gramatical.

--- mreformular.py
+++ mreformular.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+
 import tweepy
 import sys
 import urllib3.contrib.pyopenssl
@@ -9,18 +10,20 @@ urllib3.contrib.pyopenssl.inject_into_urllib3()
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-consumer_key='M5WaMYqVPjODgKMbWhVwnBRGA'
-consumer_secret='Z5xMhSrj4P1zerBuFKjAUkyBvhGeypmgRnZZ5ynTlhUlaAbx8H'
-access_token='313642562-xhZu0SdkXFWjIzLf4poaTZe9J8TPDyDYsCjowEvI'
-access_token_secret='kcW1DmYcp4EhIjX7v0v15U8l2hYDtPjfr24Mah5e7OTxo'
+pancho_consumer_key='M5WaMYqVPjODgKMbWhVwnBRGA'
+pancho_consumer_secret='Z5xMhSrj4P1zerBuFKjAUkyBvhGeypmgRnZZ5ynTlhUlaAbx8H'
+pancho_access_token='313642562-xhZu0SdkXFWjIzLf4poaTZe9J8TPDyDYsCjowEvI'
+pancho_access_token_secret='kcW1DmYcp4EhIjX7v0v15U8l2hYDtPjfr24Mah5e7OTxo'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+maquina_consumer_key='M5WaMYqVPjODgKMbWhVwnBRGA'
+maquina_consumer_secret='Z5xMhSrj4P1zerBuFKjAUkyBvhGeypmgRnZZ5ynTlhUlaAbx8H'
+maquina_access_token='313642562-xhZu0SdkXFWjIzLf4poaTZe9J8TPDyDYsCjowEvI'
+maquina_access_token_secret='kcW1DmYcp4EhIjX7v0v15U8l2hYDtPjfr24Mah5e7OTxo'
+
+auth = tweepy.OAuthHandler(pancho_consumer_key,pancho_consumer_secret)
+auth.set_access_token(pancho_access_token, pancho_access_token_secret)
 
 api = tweepy.API(auth)
-
-
-palabras=['hola','gato','nombre','tonto','abril','daniel','sol','javiera','dos','diez']
 
 correcto=0;
 seleccion=[]
@@ -28,36 +31,59 @@ seleccion=[]
 t_inicio=int(time.time())
 t_actual=int(time.time())
 contador=0
+
+#codigo principal
 while (t_actual-t_inicio<30):
-	x=randint(0,9)
-	#print x
-	for tweet in tweepy.Cursor(api.search,q=palabras[x],count=1,result_type="recent",include_entities=True,lang="es").items(1):
-		frase=str((tweet.text)).split()
-		#print frase
-		for palabra in frase:
-			#print len (palabra)
-			if len(palabra)>=4:
-				for letra in palabra:
-					if letra.lower()>='a'and letra.lower()<='z':
-						#seleccion=seleccion+[palabra]
-						correcto=1
-					else:
-						correcto=0
-						break
-			else:
-				correcto=0
-			if correcto:
-				print palabra
-				correcto=0
-				contador+=1
+ 	cantidad_lineas=0
+ 	archivo=open('lol.txt')
+ 	for linea in archivo:
+ 		cantidad_lineas=cantidad_lineas+1
+ 		#print linea.split(':')
+ 	#print cantidad_lineas
+ 	
+ 	x=randint(0,cantidad_lineas-1)
+ 	#print x
+ 	archivo.close()
+ 	contador_x=0
+ 	
+ 	archivo=open('lol.txt')
+ 	for linea in archivo:
+ 		if contador_x==x:
+ 			linea_sel=linea.split(':')
+ 			break
+ 		else:
+ 			contador_x=contador_x+1
+ 	print linea_sel
+	
+	listo=0
+	while(listo==0):
+		y=randint(0,len(linea_sel)-1)
+		palabra_sel=linea_sel[y]
+		if palabra_sel.lower()>='a'and palabra_sel.lower()<='z':
+			listo=1
+		else:
+			listo=0
+	
+	print 'palabra seleccionada:  ' ,palabra_sel, y 
 
-	t_actual=time.time();
-
-print contador
-
-
-
-			
-
-
-
+	for tweet in tweepy.Cursor(api.search,q=palabra_sel,count=1,result_type="recent",include_entities=True,lang="es").items(1):
+  	 	frase=str((tweet.text)).split()
+  	 	#print frase
+  	 	for palabra in frase:
+  	 		#print len (palabra)
+  	 		if len(palabra)>=2:
+  	 			for letra in palabra:
+  	 				if letra.lower()>='a'and letra.lower()<='z':
+  						#seleccion=seleccion+[palabra]
+  	 					correcto=1
+  	 				else:
+  	 					correcto=0
+  	 					break
+  	 		else:
+  	 			correcto=0
+  	 		if correcto:
+  	 			print palabra
+  	 			correcto=0
+	 			#contador+=1
+  	t_actual=time.time()
+	#print contador
